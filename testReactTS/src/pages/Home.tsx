180e441351701b1ae04b6b86f2dd2ae2f809e376
NONAMEGG <tyuutyuavin@gmail.com>
2025-06-04 22:18:42 +0300

    added component for table rows

--- testReactTS/src/pages/Home.tsx
+++ testReactTS/src/pages/Home.tsx
@@ -9,7 +9,7 @@ import {TaskPriority} from '../types/taskPriority';
 import {TaskStatus} from '../types/taskStatus';
 import type { TTaskPriority } from '../types/taskPriority';
 import type { TTaskStatus } from '../types/taskStatus';
-
+import TaskTableRow from '../components/TaskTableRow';
 import {
    Fab, Chip, Box, Table, TableBody, TableCell, TableContainer,
    TablePagination, TableRow, Paper, Tooltip, Checkbox
@@ -147,60 +147,15 @@ const Home = () => {
                 const labelId = `enhanced-table-checkbox-${index}`;
 
                 return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
+                  <TaskTableRow
                     key={row.id}
-                    selected={isItemSelected}
-                    sx={{ cursor: 'pointer' }}
-                    onDoubleClick={(event) => handleClick(event, row.id)}
-                  >
-                    <TableCell padding="checkbox"
-                      onClick={(event) => handleCheckBoxClick(event, row.id)}>
-                      <Checkbox
-                        color="primary"
-                        checked={isItemSelected}
-                        inputProps={{
-                          'aria-labelledby': labelId,
-                        }}
-
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                    >
-                      {
-                      row.title && row.title.length > 20 ?
-                    row.description.substring(0, 10).concat('...') : row.title
-                      }
-                    </TableCell>
-                    <TableCell align="left">{
-                      row.description && row.description.length > 20 ?
-                    row.description.substring(0, 30).concat('...') : row.description
-                    }</TableCell>
-                    <TableCell align="center">{row.createdAt.toLocaleString()}</TableCell>
-                    <TableCell align="center">
-                      <Chip color={
-                        row.status === 'PENDING' ? 'default' :
-                          row.status === 'IN_PROGRESS' ? 'secondary' : 'success'
-                      }
-                        label={row.status} variant="outlined" />
-                    </TableCell>
-                    <TableCell align="center">
-                      <Chip color={
-                        row.priority === 'HIGH' ? 'error' :
-                          row.priority === 'MEDIUM' ? 'warning' : 'primary'
-                      }
-                        label={row.priority} variant="outlined" />
-
-                    </TableCell>
-                  </TableRow>
-                );
+                    row={row}
+                    isItemSelected={isItemSelected}
+                    labelId={labelId}
+                    handleClick={handleClick}
+                    handleCheckBoxClick={handleCheckBoxClick}
+                  />
+               );
               })}
               {emptyRows > 0 && (
                 <TableRow
